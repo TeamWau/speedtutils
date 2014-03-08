8393d5294783b3807309b13c43ba434cedf8c5fa
fauxm <fauxm@enfan.org>
2014-03-08 13:14:29 -0500

    added support for any string

--- src/yes/yes.c
+++ src/yes/yes.c
@@ -3,9 +3,17 @@
 #define PROGRAM_NAME "yes"
 #define AUTHORS "fauxm"
 
-int main() {
-    while( 1 ) {
-        printf( "y\n" );
+int main( int argc, char** argv ) {
+    if( argc == 1 ) {
+        while( 1 ) {
+            printf( "y\n" );
+        }
+    }
+
+    else {
+        while( 1 ) {
+            printf( "%s\n", argv[1] );
+        }
     }
     return 0;
 }
