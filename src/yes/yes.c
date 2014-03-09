3129de698fb571e3e9e6addd13ad251aa3030d3f
fauxm <fauxm@enfan.org>
2014-03-09 10:28:21 -0400

    made changes to yes.c

--- src/yes/yes.c
+++ src/yes/yes.c
@@ -3,16 +3,12 @@
 #define PROGRAM_NAME "yes"
 #define AUTHORS "fauxm"
 
+//todo: add support for other strings
+
 int main( int argc, char** argv ) {
     if( argc == 1 ) {
         while( 1 ) {
             printf( "y\n" );
-        }
-    }
-
-    else {
-        while( 1 ) {
-            printf( "%s\n", argv[1] );
         }
     }
     return 0;
