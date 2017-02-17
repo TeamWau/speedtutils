9157aab5a6e48279d933e0af13f5fe50c97cf7d9
fauxm <fauxm1135@gmail.com>
2017-02-16 22:13:12 -0500

    1TBS'd mv.c and cp.c

--- src/mv/mv.c
+++ src/mv/mv.c
@@ -16,42 +16,40 @@
     TODO: Check if destination file exists in order to prevent accidental overwrites
 */
 
-int main( int argc, char** argv ) {
-
-    if( argc < 2 ) {
-        printf("%s - memory mapping equivalent to `mv'\nProvides high-speed file move\n\nError: please specify a file name!\nUsage: %s <original> <new>\n", argv[0], argv[0] );
+int main(int argc, char** argv) 
+{
+    if (argc < 2) {
+        printf("%s - memory mapping equivalent to `mv'\nProvides high-speed file move\n\nError: please specify a file name!\nUsage: %s <original> <new>\n", argv[0], argv[0]);
         return 1;
     }
 
     char *buf;
     struct stat s;
-    int infile = open( argv[1], O_RDONLY ); //explicitly define the input file as read only
+    int infile = open(argv[1], O_RDONLY); //explicitly define the input file as read only
 
-    if ( infile < 0 ) {
-        perror( "Error" ); 
-        printf( "\nAbort! Abort!\n" );
+    if (infile < 0) {
+        perror("Error"); 
+        printf("\nAbort! Abort!\n");
         return 2;
     }
-    FILE* outfile = fopen( argv[2], "w" );
+    FILE* outfile = fopen(argv[2], "w");
 
-    fstat( infile, &s ); //get data about our input file, store it in the struct
+    fstat(infile, &s); //get data about our input file, store it in the struct
 
     /* map the struct (which now contains our file data) to memory.
      * We make our mapping read only and private (since it won't change)
      * No offsets.
      */
-    buf = mmap( 0, s.st_size, PROT_READ, MAP_SHARED, infile, 0 );
+    buf = mmap(0, s.st_size, PROT_READ, MAP_SHARED, infile, 0);
 
-    if( buf != ( void* ) - 1 ) {
-        fwrite_unlocked( buf, s.st_size, 1, outfile ); //write everything from mapped memory to the output file
-        munmap( buf, s.st_size ); //free up the memory we mapped
+    if (buf != (void*) - 1) {
+        fwrite_unlocked(buf, s.st_size, 1, outfile); //write everything from mapped memory to the output file
+        munmap(buf, s.st_size); //free up the memory we mapped
     }
 
-    if(remove (argv[1]) == 0){
+    if (remove (argv[1]) == 0) {
         return 0;
-    }
-
-    else {
+    } else {
         perror(argv[1]);
         return 1;
     }
