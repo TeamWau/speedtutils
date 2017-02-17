27983b1cd87cb073e7c9b7ca51d61cc986f8b20f
fauxm <fauxm1135@gmail.com>
2017-02-16 21:57:40 -0500

    Changed style to 1TBS for yes.c. More to come

--- src/yes/yes.c
+++ src/yes/yes.c
@@ -3,26 +3,23 @@
 #define PROGRAM_NAME "yes"
 #define AUTHORS "fauxm"
 
-int main( int argc, char** argv ) {
-    if( argc == 1 ) {
-        while( 1 ) {
-            printf( "y\n" );
+int main(int argc, char** argv) 
+{
+    if (argc == 1) {
+        while (1) {
+            printf("y\n");
         }
-    }
-    
-    else if( argc > 1 ) {
+    } else if (argc > 1) {
         int count;
-        while( 1 ) {
-            for( count = 1; count < argc; count++ ) {
-                if( count == argc - 1 ) {
-                    printf( "%s", argv[count] );
-                }
-                else {
-                    printf( "%s ", argv[count] );
+        while (1) {
+            for (count = 1; count < argc; count++) {
+                if (count == argc - 1) {
+                    printf("%s", argv[count]);
+                } else {
+                    printf("%s ", argv[count]);
                 }
             }
-
-            printf( "\n" );
+            printf("\n");
         }
     }
 
