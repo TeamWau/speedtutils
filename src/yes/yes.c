35b0666debd6dc5611a82b9a1ce5593754ea7277
fauxm <fauxm1135@gmail.com>
2017-02-16 21:16:00 -0500

    Revert "Added support for arbitrary strings for yes. Also, first commit in three years, jeez!"

--- src/yes/yes.c
+++ src/yes/yes.c
@@ -3,28 +3,13 @@
 #define PROGRAM_NAME "yes"
 #define AUTHORS "fauxm"
 
+//todo: add support for other strings
+
 int main( int argc, char** argv ) {
     if( argc == 1 ) {
         while( 1 ) {
             printf( "y\n" );
         }
     }
-    
-    else if( argc > 1 ) {
-        int count;
-        while( 1 ) {
-            for( count = 1; count < argc; count++ ) {
-                if( count == argc - 1 ) {
-                    printf("%s", argv[count]);
-                }
-                else {
-                    printf( "%s ", argv[count] );
-                }
-            }
-
-            printf("\n");
-        }
-    }
-
     return 0;
 }
